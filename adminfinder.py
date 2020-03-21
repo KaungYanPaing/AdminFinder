890727ec4250380f993dc7a33bc7d45c0cc27bb7
KaungYanPaing <51271827+KaungYanPaing@users.noreply.github.com>
2020-03-22 02:07:34 +0630

    Add files via upload

---
+++ adminfinder.py
@@ -0,0 +1,53 @@
+import requests
+
+def main():
+	banner()
+	bruteforcer()
+	end()
+	while True:
+		process = input('Do u want to continue (yes/no):: ')
+		if (process == 'yes' or process == 'y' or process == 'Yes' or process == 'Y'):
+			banner()
+			bruteforcer()
+			continue
+		elif (process == 'no' or process == 'n' or process == 'No' or process == 'N'):
+			break
+		else:
+			print('Enter either [yes] or [no]:: ')
+	end()
+			
+def banner():
+	print('\n')
+	print('\t ==--==--==--==--==--==--==--==--==--==')
+	print('\t ==--==--==--==--==--==--==--==--==--==')
+	print('\t ==--   Admin Login Finder V 1.0   --==')
+	print('\t ==--        Author : T0x1c        --==')
+	print('\t ==--==--==--==--==--==--==--==--==--==')
+	print('\t ==--==--==--==--==--==--==--==--==--==')
+	print('\n')
+	print('\t eg - Enter your target site :: https://www.target.com/')
+	print('\n')
+
+	
+def bruteforcer():
+
+	target = input(' Enter your target site :: ')
+
+	adminlist = open('admin_wordlist.txt')
+
+	for x in adminlist:
+		r = requests.get(target+x.strip())
+
+		if (r.status_code == 200):
+			print('[*] Found >>'+target+str(x))
+			print('******Login Found!!******')
+			print()
+		else:
+			print('[-]'+target+str(x)+'//----Admin Login Not Found----//' )
+	print('\n')
+
+def end():
+	print('---------------------------------------------------------------------------')
+
+
+if __name__ == '__main__':main()
